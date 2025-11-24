cf45a4ea3d65e4f5064f94fc0a9bd763669417ff
Martin dos Santos <martunahuel@hotmail.com>
2025-11-24 19:11:37 -0300

    masomenos quedando salvo el main que hay que cambiar para corregir y posibilitar la inyeccion

--- src/main/java/dondeestas/DondeEstasApplication.java
+++ src/main/java/dondeestas/DondeEstasApplication.java
@@ -1,11 +1,28 @@
 package dondeestas;
 
+import dondeestas.service.MascotaService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import telegram.DondeEstasBot;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class DondeEstasApplication {
     public static void main(String[] args) {
         SpringApplication.run(DondeEstasApplication.class, args);
+
+        String botToken="8480878618:AAGyi3cGUpz6XuGUz18YlNtvgNvj9444jO4";
+
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new DondeEstasBot(botToken,));
+            System.out.println("Bot iniciado correctamente!");
+            // Ensure this prcess wait forever
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
